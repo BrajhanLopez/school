082957f4e0d60eddc0991ac07b187e9f3c8014de
BrajhanLop <bralopezsuasnabar@gmail.com>
2022-06-19 20:08:12 -0500

    iniciando repositorio web school

---
+++ assets/js/index.js
@@ -0,0 +1,7 @@
+const ul = document.querySelector('.ul')
+const hamburguer =  document.querySelector('.hamburguer')
+
+hamburguer.addEventListener('click', ()=> {
+
+    ul.classList.toggle('ul2')
+})
